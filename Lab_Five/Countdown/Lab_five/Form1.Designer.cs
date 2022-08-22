8af5b1606a75118e8904a48febb83f485b354c76
OldinPadawan <diyandmakers@gmail.com>
2022-08-22 13:33:29 +0300

    finished countdown timer

--- Lab_Five/Countdown/Lab_five/Form1.Designer.cs
+++ Lab_Five/Countdown/Lab_five/Form1.Designer.cs
@@ -30,14 +30,19 @@
         {
             this.StartButton = new System.Windows.Forms.Button();
             this.CountDown = new System.Windows.Forms.TextBox();
+            this.imagesBox = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusBox = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imagesBox)).BeginInit();
             this.SuspendLayout();
             // 
             // StartButton
             // 
             this.StartButton.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.StartButton.Location = new System.Drawing.Point(543, 196);
+            this.StartButton.Location = new System.Drawing.Point(524, 121);
             this.StartButton.Name = "StartButton";
-            this.StartButton.Size = new System.Drawing.Size(212, 43);
+            this.StartButton.Size = new System.Drawing.Size(264, 70);
             this.StartButton.TabIndex = 0;
             this.StartButton.Text = "Старт!";
             this.StartButton.UseVisualStyleBackColor = true;
@@ -45,20 +50,57 @@
             // 
             // CountDown
             // 
-            this.CountDown.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.CountDown.Location = new System.Drawing.Point(543, 128);
+            this.CountDown.Font = new System.Drawing.Font("Microsoft JhengHei", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CountDown.Location = new System.Drawing.Point(524, 24);
+            this.CountDown.Multiline = true;
             this.CountDown.Name = "CountDown";
             this.CountDown.ReadOnly = true;
-            this.CountDown.Size = new System.Drawing.Size(212, 43);
+            this.CountDown.Size = new System.Drawing.Size(264, 82);
             this.CountDown.TabIndex = 2;
-            this.CountDown.Text = "00:00:00";
+            this.CountDown.Text = "00:00";
             this.CountDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // imagesBox
+            // 
+            this.imagesBox.Location = new System.Drawing.Point(12, 12);
+            this.imagesBox.Name = "imagesBox";
+            this.imagesBox.Size = new System.Drawing.Size(480, 229);
+            this.imagesBox.TabIndex = 3;
+            this.imagesBox.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 247);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(280, 23);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // statusBox
+            // 
+            this.statusBox.Location = new System.Drawing.Point(331, 247);
+            this.statusBox.Multiline = true;
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(161, 37);
+            this.statusBox.TabIndex = 5;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(524, 269);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(264, 169);
+            this.resultBox.TabIndex = 6;
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.imagesBox);
             this.Controls.Add(this.CountDown);
             this.Controls.Add(this.StartButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -67,6 +109,7 @@
             this.Text = "Game";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GameForm_FormClosing);
             this.Load += new System.EventHandler(this.GameForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imagesBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,5 +119,9 @@
 
         private Button StartButton;
         private TextBox CountDown;
+        private PictureBox imagesBox;
+        private ComboBox comboBox1;
+        private TextBox statusBox;
+        private TextBox resultBox;
     }
 }
--- Lab_Five/Countdown/Lab_five/Form1.cs
+++ Lab_Five/Countdown/Lab_five/Form1.cs
@@ -1,25 +1,56 @@
+using System.Timers;
+
 namespace Lab_five
 {
     public partial class GameForm : Form
     {
+        private System.Windows.Forms.Timer timer;
+        TimeSpan Countdown_time = TimeSpan.FromMinutes(3);
+        private string[] allFiles = Directory.GetFiles(@"G:\Учеба второй курс\C# projects\Lab_Five\images");
+        private List<string> imagesName = new List<string>();
         public GameForm()
         {
             InitializeComponent();
+            foreach (string name in allFiles)
+            {
+                imagesName.Add(System.IO.Path.GetFileNameWithoutExtension(name));
+            }
+
+
         }
 
         private void StartButton_Click(object sender, EventArgs e)
         {
-
+            timer.Start();
         }
 
         private void GameForm_Load(object sender, EventArgs e)
         {
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 1000;
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (Countdown_time.TotalSeconds >= 0)
+            {              
+                CountDown.Text = Countdown_time.ToString("mm\\:ss");
+                Countdown_time = Countdown_time.Subtract(TimeSpan.FromSeconds(1));
+            } else
+            {
+                timer.Stop();
+            }
 
         }
 
+
+
         private void GameForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-
+            timer.Stop();
+            Application.DoEvents();
         }
     }
 }
