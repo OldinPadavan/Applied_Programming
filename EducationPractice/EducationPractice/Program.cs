0f708ad4e79456120dd94184d83cc47254c5c2b5
OldinPadawan <diyandmakers@gmail.com>
2022-12-10 12:07:13 +0300

    added xml documentation in practice project

--- EducationPractice/EducationPractice/Program.cs
+++ EducationPractice/EducationPractice/Program.cs
@@ -3,21 +3,69 @@
 namespace EducationPractice
 
 {
+    /// <summary>
+    /// Testing class with basic commands
+    /// </summary>
     public class Program
     {
+        /// <value>
+        /// variable declaration for input 
+        /// </value>
+        public int input;
+
+        /// <summary>
+        /// main method
+        /// </summary>
         public static void Main()
         {
+            int x = 5;
+            Program program= new Program();
+            program.input = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hello World!");
+            //foreach (char ch in "Hello World!")
+            //{
+            //    Console.WriteLine(ch);
+            //}
+
+            Console.WriteLine(program.Sum(x));
+            Console.WriteLine(x);
+            try
+            {
+                Console.WriteLine(x / program.input);
+            } catch(DivideByZeroException exception)
+            {
+                Console.WriteLine("I tried to divided first number to zero " + " " + exception.Message);
+            }
         }
 
-        public void zuu ( )
+        /// <summary>
+        /// test summary method
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns> sum of two int value</returns>
+        public int Sum(int x)
         {
-            zuu( );
+            int g = 10;
+            x += 5;
+            return x + g;
         }
 
-        public void fuu(int a, int b)
+        /// <summary>
+        /// recursive call example
+        /// </summary>
+        public void Zuu ( )
         {
-            zuu ( );
+            Zuu( );
+        }
+
+        /// <summary>
+        /// test method with two parameters and another method call
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void Fuu(int a, int b)
+        {
+            Zuu ( );
         }
         
 
---
+++ EducationPractice/UP_Two/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+
+
+//int main() {
+//clock_t start = clock();
+//int i = 4;
+//while (i < 10000) {
+//std::cout << i << " ";
+//	i++;
+//}
+//clock_t end = clock();
+//double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+//printf("The time: %f seconds\n", seconds);
+//}
+
+int main() {
+time_t start, end;
+time(&start);
+int i = 4;
+while (i < 10000) {
+	std::cout << i << " ";
+	i++;
+}
+time(&end);
+double seconds = difftime(end, start);
+printf("The time: %f seconds\n", seconds);
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
